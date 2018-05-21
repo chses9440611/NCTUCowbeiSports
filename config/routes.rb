9165069b0160fd1c7153463bf8682540a6ca648e
chses910372 <chses910372@gmail.com>
2018-05-21 12:49:28 +0800

    fail

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/" => "home#index"
+  resources :announcements, only: [:index,:new, :create, :show, :edit, :delete] do
+	  member do 
+		  get :modal
+	  end
+  end
+
 end
---
+++ test/controllers/anncs_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AnncsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
