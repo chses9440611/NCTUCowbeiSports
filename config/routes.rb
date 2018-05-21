0b60a3a9e73773873e6169e2d60f5e92ae58c136
chses910372 <chses910372@gmail.com>
2018-05-21 20:57:02 +0800

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/" => "home#index"
+  get "/" => "home#index", as: :home
+  resources :announcements, only: [:index, :create, :update]
+  get "/anncs" => "announcements#show", as: :anncs
+  get "/anncs/add" => "announcements#new", as: :new_announcement
 end
