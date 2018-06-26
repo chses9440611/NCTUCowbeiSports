40aeb6eee1839918935f787bc48f6c4c6d4cfe59
Tsung-en Hsiao <tsn@cs.nctu.edu.tw>
2018-06-26 15:33:50 +0800

    fix migration typo

--- db/schema.rb
+++ db/schema.rb
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2018_06_25_202505) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "student_id", null: false
+    t.string "password", null: false
     t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
