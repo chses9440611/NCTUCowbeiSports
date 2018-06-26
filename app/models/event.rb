58cdb2ae95d74cce5a177e270460d5229c9b1a2b
chses910372 <chses910372@gmail.com>
2018-06-26 12:27:22 +0800

    add validation of event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
 	has_many :teams, dependent: :destroy
 	accepts_nested_attributes_for :teams, :allow_destroy => true
+
+	#validation
+	validates :name, :event_date, :max_num_team, :min_num_team, :max_num_people, :min_num_people, presence: true
 end
