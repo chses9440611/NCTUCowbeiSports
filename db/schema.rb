512c6c3b82db8eacf6963d931e8ab0d0c5ddad31
chses910372 <chses910372@gmail.com>
2018-06-27 14:00:40 +0800

    remove password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_25_202505) do
+ActiveRecord::Schema.define(version: 2018_06_27_055247) do
 
   create_table "announcements", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -49,7 +49,6 @@ ActiveRecord::Schema.define(version: 2018_06_25_202505) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "student_id", null: false
-    t.string "password", null: false
     t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
